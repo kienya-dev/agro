07ab2155ae8c8cb69108659af607b3049e7b6bc1
Kanstantsin Kiyenia <kienya.dev@gmail.com>
2023-06-09 19:11:09 +0300

    add: страницы услуги и каталог

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,39 @@
 (() => {
     "use strict";
+    document.addEventListener("DOMContentLoaded", (() => {
+        const body = document.querySelector("body");
+        body.classList.remove("preload");
+        const isWebp = () => {
+            function testWebP(callback) {
+                let webP = new Image;
+                webP.onload = webP.onerror = function() {
+                    callback(webP.height == 2);
+                };
+                webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+            }
+            testWebP((function(support) {
+                let className = support === true ? "webp" : "no-webp";
+                document.documentElement.classList.add(className);
+            }));
+        };
+        isWebp();
+        const menuToggles = document.querySelectorAll("[data-nav-button]");
+        if (menuToggles.length) {
+            const toggleMenu = toggler => {
+                const boxMenu = toggler.closest("[data-nav-box]");
+                const menu = boxMenu.querySelector("[data-nav-list]");
+                const links = boxMenu.querySelectorAll("a");
+                const icon = boxMenu.querySelector(".catalog-nav__icon");
+                toggler.classList.toggle("nav__button_active");
+                menu.classList.toggle("nav__box_active");
+                if (icon) icon.classList.toggle("catalog-nav__icon_active");
+                links.forEach((link => link.addEventListener("click", (() => {
+                    toggler.classList.remove("nav__button_active");
+                    menu.classList.remove("nav__box_active");
+                    if (icon) icon.classList.toggle("catalog-nav__icon_active");
+                }))));
+            };
+            menuToggles.forEach((toggler => toggler.addEventListener("click", (() => toggleMenu(toggler)))));
+        }
+    }));
 })();
--- src/js/app.js
+++ src/js/app.js
@@ -193,4 +193,65 @@ import "../scss/style.scss";
 /* Подключаем файлы со своим кодом */
 import "./files/script.js";
 //============================================================================================================================================================================================================================================
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body')
+
+
+    /*Возвращаем анимацию после загрузки DOM*/
+    //============================================================================================================================================================================================================================================
+    body.classList.remove('preload');
+
+    /*Background .webp*/
+    //============================================================================================================================================================================================================================================
+
+    const isWebp = () => {
+        // Проверка поддержки webp
+        function testWebP(callback) {
+            let webP = new Image();
+            webP.onload = webP.onerror = function () {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        // Добавление класса _webp или _no-webp для HTML
+        testWebP(function (support) {
+            let className = support === true ? 'webp' : 'no-webp';
+            document.documentElement.classList.add(className);
+        });
+    }
+    isWebp();
+
+    /* Burger menu*/
+    //============================================================================================================================================================================================================================================
+    const menuToggles = document.querySelectorAll('[data-nav-button]');
+
+    if (menuToggles.length) {
+
+        const toggleMenu = (toggler) => {
+            const boxMenu = toggler.closest('[data-nav-box]');
+            const menu = boxMenu.querySelector('[data-nav-list]');
+            const links = boxMenu.querySelectorAll('a');
+            const icon = boxMenu.querySelector('.catalog-nav__icon')
+            toggler.classList.toggle('nav__button_active');
+            menu.classList.toggle('nav__box_active');
+
+            if (icon) {
+                icon.classList.toggle('catalog-nav__icon_active')
+            }
+
+            links.forEach(link => link.addEventListener('click', () => {
+                toggler.classList.remove('nav__button_active');
+                menu.classList.remove('nav__box_active');
+                if (icon) {
+                    icon.classList.toggle('catalog-nav__icon_active')
+                }
+            }))
+
+        };
+
+        menuToggles.forEach(toggler => toggler.addEventListener('click', () => toggleMenu(toggler)));
+
+    }
+});
+
 
